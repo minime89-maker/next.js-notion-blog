2b09e257a4709fbd33b06dbf52137386f1340445
Minja Popovic <mini.me@mein.gmx>
2021-11-15 16:41:27 +0100

    PWA added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
 	images: {
 		domains: ['s3-us-west-2.amazonaws.com','images.unsplash.com', 'miro.medium.com', 's3.us-west-2.amazonaws.com', 'images.pexels.com', 'i.imgur.com' ]
 	},
-	
-}
+	pwa: {
+		dest: 'public',
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV !== 'development'
+		},
+	reactStrictMode: true,
+})

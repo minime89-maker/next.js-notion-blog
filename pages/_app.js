da4d761d5c9c5e379b1d2deed55035030bddbc94
Minja Popovic <mini.me@mein.gmx>
2021-11-16 08:43:11 +0100

    sw detection

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,31 @@ import "prismjs/themes/prism-tomorrow.css";
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
+import { useEffect } from 'react'
 
 Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }) {
+
+  // service worker
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <ThemeProvider attribute='class'>
         <Component {...pageProps} />

69d750ff16ef575e398009132c949f0b9f5fdf33
Minja Popovic <mini.me@mein.gmx>
2022-04-24 10:46:36 +0200

    feat: meta tags

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require('next-pwa')
 
 const production = process.env.NODE_ENV === 'production'
 
-export default withPWA({
+module.exports = withPWA({
   images: {
     domains: [
       's3-us-west-2.amazonaws.com',
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,50 @@ const MyDocument = () => {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="application-name"
+          content="Blog"
+        />
+        <meta
+          name="apple-mobile-web-app-capable"
+          content="yes"
+        />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="default"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Blog"
+        />
+        <meta
+          name="description"
+          content="Notion Blog"
+        />
+        <meta
+          name="format-detection"
+          content="telephone=no"
+        />
+        <meta
+          name="mobile-web-app-capable"
+          content="yes"
+        />
+        <meta
+          name="msapplication-config"
+          content="/icons/browserconfig.xml"
+        />
+        <meta
+          name="msapplication-TileColor"
+          content="#2B5797"
+        />
+        <meta
+          name="msapplication-tap-highlight"
+          content="no"
+        />
+        <meta
+          name="theme-color"
+          content="#000000"
+        />
         <link
           rel="manifest"
           href="/manifest.json"

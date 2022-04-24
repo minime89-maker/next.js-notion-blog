c142c77e68d1a0edb464112de37ac4e38e72244b
Minja Popovic <mini.me@mein.gmx>
2022-04-24 10:35:05 +0200

    fix: scrollToTop icon

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,38 +1,39 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react'
+import { ArrowUpCircle } from 'react-feather'
 
 const ScrollToTop = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false)
 
   const handleScroll = useCallback(() => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
-    });
-  }, []);
+    })
+  }, [])
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
       if (window.scrollY > 180) {
-        setIsScrolled(true);
+        setIsScrolled(true)
       } else if (window.scrollY < 180) {
-        setIsScrolled(false);
+        setIsScrolled(false)
       }
-    });
-  });
+    })
+  })
 
   return (
     <>
       <button
-        className="fixed bottom-0 right-0 m-4 p-2 bg-transparent border-transparent text-gray-500 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-500 rounded-full focus:outline-none"
+        className="fixed bottom-4 right-0 m-4 p-2 bg-transparent border-transparent text-gray-500 hover:text-gray-800 hover:bg-gray-200 hover:border-gray-500 rounded-full focus:outline-none"
         onClick={handleScroll}
         style={{
           display: isScrolled ? 'inline' : 'none',
         }}
       >
-        Scroll
+        <ArrowUpCircle size={26} />
       </button>
     </>
-  );
-};
+  )
+}
 
-export default ScrollToTop;
+export default ScrollToTop
